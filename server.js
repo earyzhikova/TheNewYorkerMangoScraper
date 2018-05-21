e2ceeedc60ccb3ad73927a665b3b4912870ec23d
earyzhikova <elenaryzh55@gmail.com>
2018-05-21 16:31:22 -0400

    scraped titles

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ var mongoose = require("mongoose");
 // Require request and cheerio. This makes the scraping possible
 var request = require("request");
 var cheerio = require("cheerio");
-var exphbs  = require('express-handlebars');
-var bodyParser = require('body-parser'); 
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 
 // Initialize Express
 var app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 // Use body-parser for handling form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,7 +32,7 @@ mongoose.Promise = Promise;
 mongoose.connect(MONGODB_URI);
 
 // Main route (simple Hello World Message)
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.send("Hello world");
 });
 
@@ -52,42 +52,42 @@ app.get("/", function(req, res) {
 // });
 
 // Scrape data from one site and place it into the mongodb db
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
   // Make a request for the news section of `ycombinator`
-  request("https://www.newyorker.com/news", function(error, response, html) {
+  request("https://www.newyorker.com/news", function (error, response, html) {
     // Load the html body from request into cheerio
     var $ = cheerio.load(html);
     // For each element with a "title" class
-    $("div[class^='LatestSection__listContainer']").each(function(i, element) {
+    $("div[class^='LatestSection__listContainer']").each(function (i, element) {
 
-      //each card
-      console.log($(element).children("[class^='Card__hed']").html());
+      //get each card and do something for each
+      $(element).children("div[class^='Card__theLatest']").each(function (i, articleElement) {
 
-
-      // Save the text and href of each link enclosed in the current element
-      console.log($(i).html());
-      var title = $(element).children("div").text(); //$("a[class^='Card__hed').children("a").text();
-      var link = $(element).children("h1").attr("href");
-      var article = $(element).children("h2").text();
-      // If this found element had both a title and a link
-      if (title && link && article) {
-        // Insert the data in the scrapedData db
-        db.scrapedData.insert({
-          title: title,
-          link: link,
-          article: article
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
-            console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
+        // Save the text and href of each link enclosed in the current element
+        var title = $(articleElement).find("[class^='Card__hed']").text();
+        console.log(title);
+        var link = $(articleElement).children("h1").attr("href");
+        var article = $(element).children("h2").text();
+        // If this found element had both a title and a link
+        if (title && link && article) {
+          // Insert the data in the scrapedData db
+          db.scrapedData.insert({
+            title: title,
+            link: link,
+            article: article
+          },
+            function (err, inserted) {
+              if (err) {
+                // Log the error if one is encountered during the query
+                console.log(err);
+              }
+              else {
+                // Otherwise, log the inserted data
+                console.log(inserted);
+              }
+            });
+        }
+      });
     });
   });
 
@@ -97,6 +97,6 @@ app.get("/scrape", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("App running on port 3000!");
 });
